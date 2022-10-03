2169bf4b3ecf24312feafec108e3b6438a35f067
Ben <peacepenguin@users.noreply.github.com>
2022-10-03 13:50:10 -0600

    Update buildFromSrcRpm.sh

--- buildFromSrcRpm.sh
+++ buildFromSrcRpm.sh
@@ -7,23 +7,23 @@
 #sudo dnf install cmake libva-utils libva rpm-build --assumeyes
 
 cd ~
-sudo dnf download --source mesa-libGL --assumeyes
+dnf download --source mesa-libGL --assumeyes
 
-rpm -i $srcrpmname
+#rpm -i $srcrpmname
 
 
 ## EDIT THE MESA SPEC FILE
 
-cp ~/rpmbuild/SPECS/mesa.spec ~/rpmbuild/SPECS/mesa.spec.bak
-sed -i "s*release_number = $srcrelease;*release_number = $targetrelease;*" ~/rpmbuild/SPECS/mesa.spec
+#cp ~/rpmbuild/SPECS/mesa.spec ~/rpmbuild/SPECS/mesa.spec.bak
+#sed -i "s*release_number = $srcrelease;*release_number = $targetrelease;*" ~/rpmbuild/SPECS/mesa.spec
 
-nl=$'\n'
+#nl=$'\n'
 
-sed -i 's*-Dvulkan-layers=device-select \\*-Dvulkan-layers=device-select \\'"\\${nl}"'  -Dvideo-codecs=h264dec,h264enc,h265dec,h265enc,vc1dec \\*' ~/rpmbuild/SPECS/mesa.spec
+#sed -i 's*-Dvulkan-layers=device-select \\*-Dvulkan-layers=device-select \\'"\\${nl}"'  -Dvideo-codecs=h264dec,h264enc,h265dec,h265enc,vc1dec \\*' ~/rpmbuild/SPECS/mesa.spec
 
 
-diff ~/rpmbuild/SPECS/mesa.spec.bak ~/rpmbuild/SPECS/mesa.spec --context
+#diff ~/rpmbuild/SPECS/mesa.spec.bak ~/rpmbuild/SPECS/mesa.spec --context
 
-cd ~/rpmbuild/
-rpmbuild -bb ~/rpmbuild/SPECS/mesa.spec
+#cd ~/rpmbuild/
+#rpmbuild -bb ~/rpmbuild/SPECS/mesa.spec
 
